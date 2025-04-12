2c14676a53a246dbf5535ba31177d0d08f8299f5
chesterh <nastya>
2025-04-12 18:59:21 +0300

    update

--- 01/generator_for_reading_filtering_file.py
+++ 01/generator_for_reading_filtering_file.py
@@ -1,8 +1,6 @@
 def filtered_file_reader(filename, find_words, stop_words):
-    find_words = set(word.lower() for item in find_words
-                     for word in item.split())
-    stop_words = set(word.lower() for item in stop_words
-                     for word in item.split())
+    find_words = set(word.lower() for item in find_words for word in item.split())
+    stop_words = set(word.lower() for item in stop_words for word in item.split())
     if isinstance(filename, str):
         with open(filename, "r", encoding="utf-8") as file:
             for line in file:
@@ -16,6 +14,5 @@ def filtered_file_reader(filename, find_words, stop_words):
                 yield line.strip()
     else:
         raise ValueError(
-            "Аргумент должен быть либо именем файла, "
-            "либо файловым объектом"
+            "Аргумент должен быть либо именем файла, " "либо файловым объектом"
         )
