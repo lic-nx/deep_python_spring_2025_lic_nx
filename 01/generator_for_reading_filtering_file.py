d1af8b86aeb7d61f795b6541ecec69405d9a692e
chesterh <nastya>
2025-04-12 19:06:14 +0300

    update

--- 01/generator_for_reading_filtering_file.py
+++ 01/generator_for_reading_filtering_file.py
@@ -1,8 +1,14 @@
 def filtered_file_reader(filename, find_words, stop_words):
-    find_words = set(word.lower()
-                    for item in find_words for word in item.split())
-    stop_words = set(word.lower()
-                    for item in stop_words for word in item.split())
+    find_words = set(
+        word.lower()
+        for item in find_words
+        for word in item.split()
+        )
+    stop_words = set(
+        word.lower()
+        for item in stop_words
+        for word in item.split()
+        )
     if isinstance(filename, str):
         with open(filename, "r", encoding="utf-8") as file:
             for line in file:
